4aeaf9f8bb67c373b4497fb0fdf73c7921a25a1a
Zachary Cohen <zachary@zacharyc.com>
2024-03-10 11:20:59 -0400

    Get back to scraping the site

--- webscrapper/src/webscrapper/core.clj
+++ webscrapper/src/webscrapper/core.clj
@@ -11,5 +11,18 @@
 
 (def patagonia-url "https://www.patagonia.com/store-locator/")
 
+(slurp patagonia-url)
 
-(html/html-resource (java.net.URL patagonia-url))
+;; https://github.com/practicalli/webscraper/blob/master/src/practicalli/webscraper_enlive.clj
+
+(defn website-content
+  "Get website content from a given URL
+
+  Arguments: web address as a string
+  Returns: list of html content as hash-maps"
+
+  [website-url]
+
+  (html/html-resource (java.net.URL. website-url)))
+
+#_(website-content patagonia-url)
